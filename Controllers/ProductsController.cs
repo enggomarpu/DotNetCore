dcd428b8cbd72987790722e770c07adccd550d59
Umar <enggomarpu@gmail.com>
2024-01-21 15:09:45 +0500

    first commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SKINET.Data;
+using SKINET.Entities;
+
+namespace SKINET.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController: ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+        public ProductsController(StoreContext context)
+        {
+            _storeContext = context;
+        }
+
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        {
+            return _storeContext.Products.ToList();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProductsAsync()
+        {
+            return  await _storeContext.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "this is a single product";
+        }
+
+    }
+}
---
+++ Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SKINET.Entities;
+
+namespace SKINET.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
